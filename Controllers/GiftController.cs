e40d71b0a02630155ee063ce29813c194537111f
André Dyrstad (Bouvet ASA) <ANDY@equinor.com>
2022-12-07 10:29:21 +0100

    a

--- Controllers/GiftController.cs
+++ Controllers/GiftController.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
 
 namespace SantasWishlist.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("gifts")]
     public class GiftController : ControllerBase
     {
         public GiftController() { }
 
         [HttpGet]
-        public IActionResult Get() => Ok();
+        public IActionResult GetAll() => Ok();
+
+        [HttpGet]
+        [Route("/{id}")]
+        public IActionResult Get(string id) => Ok();
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string person) => Ok();
+
+        [HttpDelete]
+        [Route("/{id}")]
+        public IActionResult Delete(string id) => Ok();
     }
 }
